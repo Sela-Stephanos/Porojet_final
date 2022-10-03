b8e56902c879b41b582dbded80d542f50887eb79
Sela-Stephanos <stephanaspro@gmail.com>
2022-10-03 09:12:42 +0200

    panier code

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'panier')]
-    public function index(): Response
+    public function index($id, SessionInterface $session, ManagerRegistry $manager): Response
     {
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id]))
+        {
+         $panier[$id]++;
+        }
+        else{$panier[$id] = 1;}
+
+        $session->get('panier', $panier);
+
         return $this->render('panier/panier.twig', [
             'controller_name' => 'PanierController',
         ]);
---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Stripe\Checkout\Session;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentController extends AbstractController
+{
+    #[Route('/payment', name: 'app_payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
+    /**
+     * @throws ApiErrorException
+     */
+    public function checkout($stripe_SK): RedirectResponse
+    {
+        Stripe::setApiKey($stripe_SK);
+
+        $session = Session::create([
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => 'T-shirt',
+                    ],
+                    'unit_amount' => 2000,
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => 'https://example.com/success',
+            'cancel_url' => 'https://example.com/cancel',
+        ]);
+        return $this->redirect($session->url, 303);
+    }
+}
